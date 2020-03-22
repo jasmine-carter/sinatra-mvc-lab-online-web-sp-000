3ac2a40ca7d8a0587eea5d87e2c242f890aa8f00
Jasmine Carter <jasmine.carter87@gmail.com>
2020-03-22 18:57:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,12 +5,10 @@ class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
-
+  
   post '/piglatinize' do
-      #do some things with the PigLatinizer class and throw results in pig_latined erb
-      text_from_user = params[:user_phrase]
-      @pig_latined_text = PigLatinizer.new(params[:user_phrase])
-      erb :pig_latined
-    end
+    text_from_user = params[:user_phrase]
+    @pig_latined_text = PigLatinizer.new(params[:user_phrase])
+    erb :pig_latined
   end
 end
